428125d88fa78f6761b1587ea7b638eb54010698
René Mayer <rene_mayer@gmx.net>
2013-06-17 11:46:51 +0200

    Jaxp insted of Jdom

---
+++ R/eAnswers.R
@@ -0,0 +1,13 @@
+
+#' @name eAnswers
+#' @title norm: items aggregated within scales 
+#' @description This data set contains an example responses to a launched questionnaire object.
+#' The data would be the output from the function calls questionnaire(), launch() and
+#' the responses assumed to be stored in an object with get.answers(). 
+#' @docType data
+#' @usage eAnswers
+#' @format a \code{data.frame} instance, 1 row per item (115 total). 
+#' @source simulated data set
+#' @examples data( eAnswers)
+#' str(eAnswers)
+NULL
